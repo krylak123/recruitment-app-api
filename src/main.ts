0772627a8b2ee388984c203548a7d407679a2e2d
Krystian Grzelak <krystian.grzelak@o2.pl>
2023-08-24 17:09:31 +0200

    feat: clear app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port: string | number = process.env['PORT'] || 3000;
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
+
 bootstrap();
