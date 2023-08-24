43d176e07b1929c99e2c89adeac1890fa411ea0a
Krystian Grzelak <krystian.grzelak@o2.pl>
2023-08-24 17:32:26 +0200

    feat: create auth and user modules

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const port: string | number = process.env['PORT'] || 3000;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(port);
 }
 
